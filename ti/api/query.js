4e95499d401fda65941c7cc14572c45bfaf74f1c
Joshua Heller <joshua@torfeld6.com>
2020-01-09 08:20:07 +0100

    Update query.js

--- ti/api/query.js
+++ ti/api/query.js
@@ -26,43 +26,45 @@ export function query(query, db = 'pitcher') {
         }
 
         fireEvent('dbFunction', {
-            db: 'pitcher',
-            pType: 'query',
-            query: query,
-        }).then(function (e) {
-            let result = [];
+                db: 'pitcher',
+                pType: 'query',
+                query: query,
+            })
+            .then(function (e) {
+                let result = [];
 
-            if (e.error) {
-                reject(new Error(e.error))
-            }
-
-            for (let i = 0; i < e.results.length; i++) {
-                let res = e.results[i];
-                let obj = {};
-
-                for (let j = 0; j < e.columns.length; j++) {
-                    let column = e.columns[j];
-
-                    if (column === 'extraField') {
-                        let o = JSON.parse(res[j])
-                        for (let n in o) {
-                            if (o.hasOwnProperty(n)) {
-                                obj[n] = o[n];
-                            }
-                        }
-                    } else {
-                        if (typeof res[j] == 'undefined') {
-                            obj[column] = null
-                        } else {
-                            obj[column] = res[j]
-                        }
-                    }
+                if (e.error) {
+                    reject(new Error(e.error))
                 }
 
-                result.push(obj)
-            }
-            cache[query] = {result: result, time: Date.now()};
-            resolve(result)
-        }).catch(reject)
+                for (let i = 0; i < e.results.length; i++) {
+                    let res = e.results[i];
+                    let obj = {};
+
+                    for (let j = 0; j < e.columns.length; j++) {
+                        let column = e.columns[j];
+
+                        if (column === 'extraField') {
+                            let o = JSON.parse(res[j])
+                            for (let n in o) {
+                                if (o.hasOwnProperty(n)) {
+                                    obj[n] = o[n];
+                                }
+                            }
+                        } else {
+                            if (typeof res[j] == 'undefined') {
+                                obj[column] = null
+                            } else {
+                                obj[column] = res[j]
+                            }
+                        }
+                    }
+
+                    result.push(obj)
+                }
+                cache[query] = {result: result, time: Date.now()};
+                resolve(result)
+            })
+            .catch(reject)
     })
 }
