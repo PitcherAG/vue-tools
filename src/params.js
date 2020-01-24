9e75424dda69be66bf0af9cd5c01dc122fd239ce
Patrick Lauber <digi@treepy.com>
2020-01-24 16:32:53 +0100

    composition api and refactor

---
+++ src/params.js
@@ -0,0 +1,46 @@
+import { createStore } from 'pinia'
+
+window.getParameters = function(text) {
+    window.params = JSON.parse(text)
+}
+
+export const useParamsStore = createStore({
+    id: 'params',
+    state: () => ({
+        account: null,
+        contacts: null,
+        salesForceUser: null,
+    }),
+    getters: {
+        locale: (state, getters) => {
+            return state.salesForceUser ? state.salesForceUser.LanguageLocaleKey.split('_').join('-') : null
+        }
+    }
+})
+
+export function loadParams() {
+    const store = useParamsStore()
+    return new Promise(resolve => {
+        if (process.env.VUE_APP_PARAMS) {
+            let preParams = JSON.parse(process.env.VUE_APP_PARAMS)
+            store.patch(preParams)
+            resolve(store.state)
+        } else if (typeof window.params === 'undefined') {
+            let interval = setInterval(() => {
+                if (typeof window.params !== 'undefined') {
+                    clearInterval(interval)
+                    store.patch(window.params)
+                    resolve(store.state)
+                }
+            }, 100)
+        } else {
+            store.patch(window.params)
+            resolve(store)
+        }
+    })
+}
+
+export function useParams() {
+    const params = useParamsStore().state
+    return { params }
+}
