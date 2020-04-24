a941529553a65f4421fb01f1ef6d44ad4c44a70d
Onur Elibol <korax11@hotmail.com>
2020-04-24 09:40:47 +0200

    no space after functions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,20 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  globals: {
-    "$": true,
-    "Ti": true,
-  },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint"
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  }
-};
+    root: true,
+    env: {
+        node: true
+    },
+    globals: {
+        $: true,
+        Ti: true
+    },
+    extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'max-len': ['error', { code: 120 }],
+        'space-before-function-paren': 0
+    }
+}
