d32b62a85b404826cd5c1302ca77b7f7a026953f
Onur Elibol <korax11@hotmail.com>
2020-04-27 11:16:20 +0200

    add bootstrapper index.file

---
+++ src/index.js
@@ -0,0 +1,20 @@
+export * from './db/contextQuery.js'
+export * from './db/query.js'
+export * from './db/sfdcField.js'
+export * from './db/sfdcLayout.js'
+export * from './db/sfdcSchema.js'
+
+export * from './i18n/date.js'
+export * from './i18n/i18n.js'
+
+export * from './config.js'
+export * from './detailing.js'
+export * from './event.js'
+export * from './files.js'
+export * from './params.js'
+export * from './platform.js'
+
+export * from './utils/link.js'
+export * from './utils/contextExec.js'
+export * from './utils/contextExec.js'
+export * from './utils/renderContext.js'
