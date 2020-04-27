4db24de43f1658a3b0f6d10fd320ca048cf4f4e9
Onur Elibol <korax11@hotmail.com>
2020-04-27 11:40:08 +0200

    add vue components & composition api set

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+// This is required to be able to use this package as an external package.
+// Otherwise it causes to throw error in the packages that are using this package as a dependency
+import Vue from 'vue'
+import VueCompositionApi from '@vue/composition-api'
+Vue.use(VueCompositionApi)
+
 export * from './db/contextQuery.js'
 export * from './db/query.js'
 export * from './db/sfdcField.js'
@@ -18,3 +24,12 @@ export * from './utils/link.js'
 export * from './utils/contextExec.js'
 export * from './utils/contextExec.js'
 export * from './utils/renderContext.js'
+
+// Vue Components
+import Calendar from './components/Calendar.vue'
+import Dropdown from './components/Dropdown.vue'
+import ObjectForm from './components/ObjectForm.vue'
+import ObjectFormField from './components/ObjectFormField.vue'
+import TransitionPage from './components/TransitionPage.vue'
+
+export { Calendar, Dropdown, ObjectForm, ObjectFormField, TransitionPage }
