e9190a1ff0dfb90a4591cb0c62cae90b09f98199
Patrick Lauber <digi@treepy.com>
2020-04-30 16:37:17 +0200

    coverage badge

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@ module.exports = {
     preset: '@vue/cli-plugin-unit-jest',
     "collectCoverage": true,
     "collectCoverageFrom": ["**/*.{js,vue}", "!**/node_modules/**"],
+    "coverageReporters": [
+        "json-summary",
+        "text",
+        "lcov"
+    ]
 }
