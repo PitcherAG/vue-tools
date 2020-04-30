313b0da159a602aa20adc0b20efff81b21da9232
Onur Elibol <korax11@hotmail.com>
2020-04-30 12:07:02 +0200

    connect initializer & function to get serverJSON

--- src/params.js
+++ src/params.js
@@ -67,3 +67,52 @@ export function useParams() {
     const params = useParamsStore().state
     return { params }
 }
+
+// For Pitcher Connect initialization
+window.setMainNav = function(lastViewedCategory) {
+    window.lastViewedCategory = lastViewedCategory
+}
+
+window.gotJSON = function(serverJSONV, documentPathV) {
+    try {
+        window.documentPath = documentPathV
+        window.serverJSON = JSON.parse(serverJSONV)
+        window.serverJSON.files.reverse()
+        window.appID = window.serverJSON.appID
+    } catch (e) {
+        window.Ti.App.fireEvent('Error', e)
+    }
+}
+
+window.loadPresentations = function() {}
+window.showUI = function() {}
+
+export function loadServerJSON() {
+    return new Promise(resolve => {
+        if (process.env.VUE_APP_PARAMS) {
+            // for testing
+            // let preParams = JSON.parse(process.env.VUE_APP_PARAMS)
+            // store.patch(preParams)
+            // resolve(store.state)
+            /*
+              TO DO:
+              Remove later
+            */
+            resolve(undefined)
+        } else if (typeof window.serverJSON === 'undefined') {
+            let count = 0
+            let interval = setInterval(() => {
+                count++
+                if (typeof window.serverJSON !== 'undefined' || count === 10) {
+                    clearInterval(interval)
+                    // store.patch(window.params)
+                    // resolve(store.state)
+                    return resolve(window.serverJSON)
+                }
+            }, 100)
+        } else {
+            // store.patch(window.params)
+            return resolve(undefined)
+        }
+    })
+}
