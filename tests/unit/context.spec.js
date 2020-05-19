089fe59e62a8ee9e9e0779062ab44270bffb5490
Patrick Lauber <digi@treepy.com>
2020-05-19 16:30:53 +0200

    if else, docs

--- tests/unit/context.spec.js
+++ tests/unit/context.spec.js
@@ -2,8 +2,16 @@ import { execBool, execString, renderContext } from '../../src/utils'
 
 describe('context', () => {
     it('renderContext', async () => {
-        const result = renderContext('{{ a }} world', { 'a': 'hello' })
+        let result = renderContext('{{ a }} world', { 'a': 'hello' })
         expect(result).toBe('hello world')
+        result = renderContext('{{ a }} world {{b}}', { 'a': 'hello', 'b':'2'})
+        expect(result).toBe('hello world 2')
+        result = renderContext('{% if a %}{{ a }} world{% endif %}', { 'a': 'hello' })
+        expect(result).toBe('hello world')
+        result = renderContext('{%if a%}{{ a }} world{%endif%}', { 'a': 'hello' })
+        expect(result).toBe('hello world')
+        result = renderContext('{{ a.join(",")}}', { 'a': [1,2] })
+        expect(result).toBe('1,2')
     })
 
     it('execBool', async () => {
