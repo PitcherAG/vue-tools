11c7502f2c2aadce297d9958c8c53c990dadc5af
Patrick Lauber <digi@treepy.com>
2020-05-28 15:51:16 +0200

    added checkbox component

---
+++ src/components/Checkbox.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="ui toggle checkbox" ref="checkbox">
+        <input type="checkbox" tabindex="0" class="hidden" v-model="interValue" >
+        <label>{{ label }}</label>
+    </div>
+</template>
+
+<script>
+    import { onMounted } from '@vue/composition-api'
+
+    export default {
+        name: "Checkbox",
+        props:{
+            label:{
+                type:String,
+            },
+            value:{
+                type:Boolean
+            }
+        },
+        setup(props, ctx){
+            const interValue = props.value
+            onMounted(()=>{
+                console.log(ctx)
+                $(ctx.refs.checkbox).checkbox('setting', 'onChange', (e) => {
+                    console.log(e)
+                    ctx.emit('input', !props.value)
+                })
+            })
+            return {interValue}
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
