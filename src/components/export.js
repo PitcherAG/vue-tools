7f560e87fc102e5cb96c97e12d5b2f7a2aaf26ed
Onur Elibol <korax11@hotmail.com>
2020-05-29 11:50:32 +0200

    export file for components

---
+++ src/components/export.js
@@ -0,0 +1,12 @@
+/* Here you have to export all the components you have created */
+
+// Vue Components
+import Calendar from './components/Calendar.vue'
+import Dropdown from './components/Dropdown.vue'
+import Numpad from './components/Numpad.vue'
+import Checkbox from './components/Checkbox.vue'
+import ObjectForm from './components/ObjectForm.vue'
+import ObjectFormField from './components/ObjectFormField.vue'
+import TransitionPage from './components/TransitionPage.vue'
+
+export { Calendar, Checkbox, Dropdown, Numpad, ObjectForm, ObjectFormField, TransitionPage }
