f1c8149dd4d85d3bd2e6335597da6cc3e8e3c440
Onur Elibol <korax11@hotmail.com>
2020-05-29 11:39:26 +0200

    update checkbox

--- src/components/Checkbox.vue
+++ src/components/Checkbox.vue
@@ -1,35 +1,30 @@
 <template>
     <div class="ui toggle checkbox" ref="checkbox">
-        <input type="checkbox" tabindex="0" class="hidden" v-model="innerValue" >
-        <label>{{ label }}</label>
+        <input type="checkbox" tabindex="0" class="hidden" :value="value" :checked="value" />
+        <label v-if="label">{{ label }}</label>
     </div>
 </template>
 
 <script>
-    import { onMounted } from '@vue/composition-api'
+import { onMounted } from '@vue/composition-api'
 
-    export default {
-        name: "Checkbox",
-        props:{
-            label:{
-                type:String,
-            },
-            value:{
-                type:Boolean
-            }
+export default {
+    name: 'Checkbox',
+    props: {
+        value: {
+            type: Boolean,
+            required: true
         },
-        setup(props, ctx){
-            const innerValue = props.value
-            onMounted(()=>{
-                $(ctx.refs.checkbox).checkbox('setting', 'onChange', (e) => {
-                    ctx.emit('input', !props.value)
-                })
-            })
-            return {innerValue}
+        label: {
+            type: String
         }
+    },
+    setup(props, ctx) {
+        onMounted(() => {
+            $(ctx.refs.checkbox).checkbox('setting', 'onChange', () => {
+                ctx.emit('input', !props.value)
+            })
+        })
     }
+}
 </script>
-
-<style scoped>
-
-</style>
