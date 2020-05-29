6b1894e49b4dea152441b725d18729c3f3aab5f8
Patrick Lauber <digi@treepy.com>
2020-05-29 17:14:26 +0200

    multiple and clearable support for dropdowns

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,16 +1,17 @@
 <template>
     <div :class="classList" ref="dropdown">
-        <input type="hidden" v-model="value" />
-        <i class="icon" :class="icon"></i>
+        <input type="hidden" v-model="value"/>
+        <i :class="icon + ' icon'"></i>
         <div class="text" v-show="value"></div>
         <div class="default" v-show="!value">{{ defaultText }}</div>
+        <i class="remove icon" style="z-index:100"></i>
         <div class="menu">
             <div
-                class="item"
-                v-for="(option, index) in list"
-                :key="index"
-                :data-value="option.value"
-                :data-text="option.text"
+                    class="item"
+                    v-for="(option, index) in list"
+                    :key="index"
+                    :data-value="option.value"
+                    :data-text="option.text"
             >
                 {{ option.text }}
             </div>
@@ -18,101 +19,144 @@
     </div>
 </template>
 <script>
-import { computed, onMounted, onUpdated } from '@vue/composition-api'
+    import { computed, onMounted, onUpdated } from '@vue/composition-api'
 
-export default {
-    props: {
-        addClass: {
-            type: String
+    export default {
+        props: {
+            addClass: {
+                type: String,
+            },
+            value: {
+                required: true,
+            },
+            defaultText: {
+                required: true,
+            },
+            options: {
+                required: true,
+            },
+            textField: {
+                default: 'text',
+            },
+            valueField: {
+                default: 'value',
+            },
+            icon: {
+                default: 'dropdown',
+            },
+            action: {
+                type: String,
+                default: 'activate',
+            },
+            setting: {
+                type: Object,
+            },
         },
-        value: {
-            required: true
-        },
-        defaultText: {
-            required: true
-        },
-        options: {
-            required: true
-        },
-        textField: {
-            default: 'text'
-        },
-        valueField: {
-            default: 'value'
-        },
-        icon: {
-            default: 'dropdown'
-        },
-        action: {
-            type: String,
-            default: 'activate'
-        },
-        setting: {
-            type: Object
-        }
-    },
 
-    setup(props, attrs) {
-        const classList = computed(() => {
-            let cls = 'ui dropdown '
-            if (props.addClass) {
-                cls += props.addClass
-            } else {
-                cls += 'selection '
-            }
-            if (props.options.length > 10) {
-                cls += ' search'
-            }
-            return cls
-        })
-
-        const list = computed(() => {
-            if (!props.options) {
-                console.error('options is null')
-            }
-
-            return props.options.map(option => {
-                if (option.constructor === Object) {
-                    return {
-                        text: option[props.textField],
-                        value: option[props.valueField]
-                    }
+        setup(props, attrs) {
+            const classList = computed(() => {
+                let cls = 'ui dropdown '
+                if (props.addClass) {
+                    cls += props.addClass
                 } else {
-                    return {
-                        text: option,
-                        value: option
-                    }
+                    cls += 'selection '
                 }
+                if (props.options.length > 10) {
+                    cls += ' search'
+                }
+                return cls
             })
-        })
 
-        const initDropdown = () => {
-            const settings = $.extend(
-                {
-                    action: props.action,
-                    onChange: function(value, text) {
-                        attrs.emit('input', value)
-                        attrs.emit('dropdown-selected', text)
+            const list = computed(() => {
+                if (!props.options) {
+                    console.error('options is null')
+                }
+
+                return props.options.map(option => {
+                    if (option.constructor === Object) {
+                        return {
+                            text: option[props.textField],
+                            value: option[props.valueField],
+                        }
+                    } else {
+                        return {
+                            text: option,
+                            value: option,
+                        }
                     }
-                },
-                props.setting
-            )
+                })
+            })
 
-            $(attrs.refs.dropdown).dropdown(settings)
-        }
-        onMounted(() => {
-            initDropdown()
-        })
-        onUpdated(() => {
-            initDropdown()
-        })
-        return { classList, list, initDropdown }
+            const initDropdown = () => {
+                const settings = $.extend(
+                    {
+                        action: props.action,
+                        onChange: function(value, text) {
+                            attrs.emit('input', value)
+                            attrs.emit('dropdown-selected', text)
+                        },
+                    },
+                    props.setting,
+                )
+
+                $(attrs.refs.dropdown).dropdown(settings)
+            }
+            onMounted(() => {
+                initDropdown()
+            })
+            onUpdated(() => {
+                initDropdown()
+            })
+            return { classList, list, initDropdown }
+        },
     }
-}
 </script>
 
 <style scoped>
-.default {
-    color: rgba(191, 191, 191, 0.87) !important;
-}
+    .default {
+        color: rgba(191, 191, 191, 0.87) !important;
+    }
+
+    .ui.clearable.selection.dropdown.labeled i.remove.icon {
+        left: auto;
+        right: 0;
+        font-size: 1em;
+    }
+
+    .ui.dropdown > .remove.icon {
+        margin: -.78571429em !important;
+        padding: .91666667em !important;
+        right: 1.5em !important;
+        top: .78571429em !important;
+        position: absolute !important;
+        opacity: .6 !important;
+        z-index: 3 !important;
+    }
+
+    .ui.dropdown > .icon:not(.dropdown){
+        margin-left: 0.2em;
+        margin-right: 0.5em;
+    }
+
+
+    .ui.multiple.dropdown > .default {
+        position: static;
+        padding: 0;
+        max-width: 100%;
+        margin: .45238095em 0 .45238095em .64285714em;
+        line-height: 1.21428571em;
+    }
+
+    .ui.clearable.dropdown .default, .ui.clearable.dropdown a:last-of-type {
+        margin-right: 0em;
+    }
+
+    .ui.dropdown > .default {
+        display: inline-block;
+        -webkit-transition: none;
+        transition: none;
+    }
+    .ui.multiple.dropdown {
+        padding-right:0.5em;
+    }
 </style>
