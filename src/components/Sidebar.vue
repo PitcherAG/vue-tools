d1eee5e355fd47680601330dcf27ff0fa641de59
Onur Elibol <korax11@hotmail.com>
2020-05-29 11:53:34 +0200

    lint

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,48 +1,46 @@
 <template>
     <!-- eslint-disable max-len -->
     <div class="ui sidebar right push wide vertical menu" ref="sidebar">
-        <slot></slot>
+        <slot />
     </div>
 </template>
 
 <script>
-    import { createStore } from 'pinia'
-    import { onMounted } from '@vue/composition-api'
+import { createStore } from 'pinia'
+import { onMounted } from '@vue/composition-api'
 
-    function useSidebarStore() {
-        return createStore({
-            id: 'sidebar',
+function useSidebarStore() {
+    return createStore({
+        id: 'sidebar',
 
-            state: () => ({
-                data: null,
-                open: false,
-            }),
-            actions: {
-                toggle() {
-                    $('.ui.sidebar').sidebar('toggle')
-                },
-                show() {
-                    $('.ui.sidebar').sidebar('show')
-                },
-                hide() {
-                    $('.ui.sidebar').sidebar('hide')
-                },
+        state: () => ({
+            data: null,
+            open: false
+        }),
+        actions: {
+            toggle() {
+                $('.ui.sidebar').sidebar('toggle')
             },
-        })()
-    }
+            show() {
+                $('.ui.sidebar').sidebar('show')
+            },
+            hide() {
+                $('.ui.sidebar').sidebar('hide')
+            }
+        }
+    })()
+}
 
-    export default {
-        name: 'Sidebar',
-        setup(props, ctx) {
-            const store = useSidebarStore()
-            onMounted(() => {
-                $(ctx.refs.sidebar).sidebar('setting', 'onChange', () => {
-                    store.state.open = !store.state.open
-                })
+export default {
+    name: 'Sidebar',
+    setup(props, ctx) {
+        const store = useSidebarStore()
+        onMounted(() => {
+            $(ctx.refs.sidebar).sidebar('setting', 'onChange', () => {
+                store.state.open = !store.state.open
             })
-        },
-        useSidebarStore,
-    }
+        })
+    },
+    useSidebarStore
+}
 </script>
-
-<style scoped></style>
