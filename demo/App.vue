f22221e27085f754e51e2450c4b7d370440f0fa9
Onur Elibol <korax11@hotmail.com>
2020-06-03 14:56:53 +0200

    add simple demo project

---
+++ demo/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="app" class="px-4">
+        <Demo />
+    </div>
+</template>
+
+<script>
+import Demo from './Demo'
+
+export default {
+    name: 'app',
+    components: {
+        Demo
+    }
+}
+</script>
+
+<style lang="scss">
+#demo-app {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: left;
+    color: #2c3e50;
+    padding-top: 60px;
+    width: 100%;
+    overflow: hidden;
+    max-width: 100%;
+}
+
+body {
+    background-color: rgb(249, 249, 250);
+}
+</style>
