d546f48ec7cab3676a01f3c8fb3aefcdb037d3ab
Onur Elibol <korax11@hotmail.com>
2020-06-03 14:57:01 +0200

    add vue config for demo project

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    lintOnSave: false,
+    pages: {
+        demo: {
+            // entry for the page
+            entry: 'demo/main.js',
+            // the source template
+            template: 'demo/index.html'
+        }
+    }
+}
