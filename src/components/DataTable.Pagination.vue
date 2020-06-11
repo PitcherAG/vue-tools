62b6b28f512f0fc9ad94d310964881715aed20d0
Onur Elibol <korax11@hotmail.com>
2020-06-11 17:33:46 +0200

    Split pagination template

---
+++ src/components/DataTable.Pagination.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="ui pagination menu">
+        <a class="item icon" :class="{ disabled: pagination.currentPage === 1 }" @click="paginate(1)">
+            <i class="icon angle double left" />
+        </a>
+        <a
+            class="item icon"
+            :class="{ disabled: pagination.currentPage === 1 }"
+            @click="paginate(pagination.currentPage - 1)"
+        >
+            <i class="icon chevron left" />
+        </a>
+        <a
+            v-for="(p, i) in pagination.pages"
+            class="item"
+            :key="i"
+            :class="{ active: p === pagination.currentPage }"
+            @click="paginate(p)"
+        >
+            {{ p }}
+        </a>
+        <a
+            class="item icon"
+            :class="{ disabled: pagination.totalPages === pagination.currentPage }"
+            @click="paginate(pagination.currentPage + 1)"
+        >
+            <i class="icon chevron right" />
+        </a>
+        <a
+            class="item icon"
+            :class="{ disabled: pagination.totalPages === pagination.currentPage }"
+            @click="paginate(pagination.totalPages)"
+        >
+            <i class="icon angle double right" />
+        </a>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    props: {
+        pagination: {
+            type: Object,
+            required: true
+        },
+        paginate: {
+            type: Function
+        }
+    }
+})
+</script>
