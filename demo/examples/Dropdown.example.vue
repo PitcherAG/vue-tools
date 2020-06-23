60c9d3b16d1a2b23a2e5b1bf89548f89f34f485e
Onur Elibol <korax11@hotmail.com>
2020-06-23 14:07:40 +0300

    create dropdown demo

---
+++ demo/examples/Dropdown.example.vue
@@ -0,0 +1,19 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+import { defineComponent, onMounted } from "@vue/composition-api";
+
+export default defineComponent({
+    props: {},
+    setup() {
+        const data = "test";
+
+        return { data };
+    }
+});
+</script>
+
+<style lang="scss">
+</style>
