fd7a68f46177677e3c7b7430247bf1f7bdc0a330
Onur Elibol <korax11@hotmail.com>
2020-06-23 14:09:00 +0300

    WIP

--- demo/examples/Dropdown.example.vue
+++ demo/examples/Dropdown.example.vue
@@ -1,19 +1,50 @@
 <template>
-    <div></div>
+    <div>
+        <h3>Dropdown</h3>
+        <div class="ui divider" />
+        <Dropdown v-model="recordTypeSaved" :items="recordTypes" :default-text="recordTypes[0].text" />
+        <br />
+        <br />
+    </div>
 </template>
 
 <script>
-import { defineComponent, onMounted } from "@vue/composition-api";
+import Dropdown from '@/components/Dropdown'
+import { reactive, toRefs } from '@vue/composition-api'
 
-export default defineComponent({
+export default {
+    components: {
+        Dropdown
+    },
     props: {},
     setup() {
-        const data = "test";
+        const state = reactive({
+            recordTypeSaved: 'zurich',
+            recordTypes: [
+                {
+                    text: 'Zürich',
+                    value: 'zurich'
+                },
+                {
+                    text: 'Aargau',
+                    value: 'aargau'
+                },
+                {
+                    text: 'Ticino',
+                    value: 'ticino'
+                },
+                {
+                    text: 'Bern',
+                    value: 'bern'
+                },
+                {
+                    text: 'Lucerne',
+                    value: 'lucerne'
+                }
+            ]
+        })
 
-        return { data };
+        return { ...toRefs(state) }
     }
-});
+}
 </script>
-
-<style lang="scss">
-</style>
