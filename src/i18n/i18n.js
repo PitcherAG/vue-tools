2b381fe3931ddbdd957224080be3ebef3db7db60
Joshua Heller <joshua@torfeld6.com>
2020-06-25 16:16:35 +0200

    finish up

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,116 +1,95 @@
-import { createStore } from 'pinia'
-import { getTranslationIndex } from './plurals'
-import { renderSimpleContext } from '../utils'
+import {createStore} from 'pinia'
+import {getTranslationIndex} from './plurals'
+import {renderSimpleContext} from '../utils'
 
-const impactLanguageMap = {
-    AUSDE: 'de',
-    BI: 'id',
-    BR: 'pt',
-    BU: 'bg',
-    CAFR: 'fr',
-    CZ: 'cs',
-    DAN: 'da',
-    ESMX: 'es',
-    EST: 'et',
-    JP: 'ja',
-    LET: 'lv',
-    PO: 'pl',
-    PRT: 'pt',
-    SCH: 'zh-CN',
-    TCH: 'zh-TW',
-    UA: 'uk',
-    VN: 'vi'
-}
-
-const state = {
-    availableLanguages: {
-        en: 'English'
-    },
+const defaultOptions = {
+    availableLanguages: {en: 'English'},
     locale: 'en',
-    messages: {}
-}
-
-if (process.env.VUE_APP_LANGUAGES) {
-    Object.assign(state.availableLanguages, JSON.parse(process.env.VUE_APP_LANGUAGES))
+    messages: {en: {}},
 }
 
 export const useI18nStore = createStore({
     id: 'i18n',
-    state: () => ({ ...state }),
+    state: () => defaultOptions,
     actions: {
-        setImpactLanguage: async function(lang) {
-            if (impactLanguageMap[lang]) {
-                lang = impactLanguageMap[lang]
-            } else {
-                lang = lang.toLowerCase()
-            }
-
-            await this.setLanguage(lang)
-        },
-        setLanguage: async function(lang) {
+        setLanguage: async function (lang, dir = 'translations', app = 'app') {
             if (!this.state.availableLanguages[lang]) {
                 throw new Error('invalid language')
             }
 
             if (lang == 'en') {
                 this.state.locale = lang
-                this.state.messages = {}
                 return
             }
 
-            const response = await fetch(`translations/${lang}/app.json`)
+            const response = await fetch(`${dir}/${lang}/${app}.json`)
+            this.state.messages[lang] = await response.json()
             this.state.locale = lang
-            this.state.messages = await response.json()
-        }
-    }
+        },
+    },
 })
 
-export function trans(key, n = 0, context) {
+export function trans(msgid, n = 0, placeholders) {
+    if (msgid == '') {
+        return msgid
+    }
+
     const store = useI18nStore()
+    const language = store.state.locale
+    const translations = store.state.messages[language] || store.state.messages[language.split(/[_-]/)[0]]
 
-    if (!store.state.messages[store.state.locale]) {
-        return key
+    let translated
+
+    if (translations) {
+        translated = translations[msgid.trim()]
+
+        if (translated) {
+            if (Array.isArray(translated)) {
+                const index = getTranslationIndex(language, n)
+                translated = translated[index]
+            }
+        } else {
+            console.warn(`Untranslated ${language} key found: ${msgid}`)
+        }
+    } else {
+        console.warn(`No translations found for ${language}`)
     }
 
-    let result = store.state.messages[store.state.locale][key]
-
-    // plural
-    if (Array.isArray(result)) {
-        const index = getTranslationIndex(store.state.locale, n)
-        result = result[index]
+    if (!translated) {
+        translated = msgid
     }
 
-    // empty
-    if (!result) {
-        result = key
+    if (placeholders && translated.indexOf('{') > -1) {
+        translated = renderSimpleContext(translated, placeholders)
     }
 
-    // replace placeholders
-    if (context || result.indexOf('{') > -1) {
-        result = renderSimpleContext(result, context)
-    }
-
-    return result
+    return translated
 }
 
-window.$gettext = function(msgid, context) {
+window.$gettext = function (msgid, context) {
     return trans(msgid, 1, context)
 }
 
-window.translateUI = function(json) {
+window.translateUI = function (json) {
     console.warn('not implemented', JSON.parse(json))
 }
 
-window.$t = function(msgid, context) {
+window.$t = function (msgid, context) {
     return trans(msgid, 1, context)
 }
 
-window.$ngettext = function(msgid, n, context) {
+window.$ngettext = function (msgid, n, context) {
     return trans(msgid, n, context)
 }
 
 export function TranslationPlugin(Vue, options = {}) {
-    options = Object.assign({}, state, options)
+    Object.keys(options).forEach(key => {
+        if (Object.keys(defaultOptions).indexOf(key) === -1) {
+            throw new Error(`${key} is an invalid option for the translate plugin.`)
+        }
+    })
+
+    options = Object.assign(defaultOptions, options)
 
     const store = useI18nStore()
     store.patch(options)
