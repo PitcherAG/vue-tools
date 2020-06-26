663369d200187914c743f0c4a8011077ea39e045
Patrick Lauber <digi@treepy.com>
2020-06-26 14:39:45 +0200

    pinia is goooone

--- src/db/sfdcSchema.js
+++ src/db/sfdcSchema.js
@@ -1,11 +1,18 @@
-import { createStore } from 'pinia'
 import { getSchema } from '../app'
 import { Field } from './sfdcField'
 
-export const useSchemaStore = createStore({
+import Vue from 'vue'
+
+const s = {
     id: 'schema',
-    state: () => ({})
-})
+    state: {}
+}
+
+const store = Vue.observable(s)
+
+export const useSchemaStore = () => {
+    return store
+}
 
 export async function loadSchema(objectName) {
     const store = useSchemaStore()
@@ -13,10 +20,8 @@ export async function loadSchema(objectName) {
         return store.state[objectName]
     } else {
         const result = await getSchema(objectName)
-        const patch = {}
-        patch[objectName] = result
-        store.patch(patch)
-        return result
+        store.state[objectName] = result
+        return store.state[objectName]
     }
 }
 
--- src/params.js
+++ src/params.js
@@ -1,6 +1,7 @@
-import { createStore } from 'pinia'
 import { fireEvent } from './event'
 import { waitForWindowProp } from './utils'
+import Vue from 'vue'
+import { computed } from '@vue/composition-api'
 
 /** For Pitcher Impact
  |--------------------------------------------------
@@ -12,62 +13,62 @@ window.getParameters = function(text) {
     window.params = JSON.parse(text)
 }
 
-// params store
-export const useParamsStore = createStore({
+const s = {
     id: 'params',
-    state: () => ({
+    state: {
         account: null,
         contacts: null,
         salesForceUser: null,
         user: null
-        // locale: computed(() => state.salesForceUser ? state.salesForceUser.LanguageLocaleKey : null)
-    }),
-    actions: {},
-    getters: {
-        locale(state) {
-            if (state.salesForceUser) {
-                return state.salesForceUser.LanguageLocaleKey.split('_').join('-')
-            }
-            if (state.user) {
-                return state.user.LanguageLocaleKey.split('_').join('-')
-            }
-        },
-
-        language(state) {
-            if (state.salesForceUser) {
-                return state.salesForceUser.LanguageLocaleKey.split('_')[0].toLowerCase()
-            }
-            if (state.user) {
-                return state.user.LanguageLocaleKey.split('_')[0].toLowerCase()
-            }
-        },
-        context(state) {
-            return {
-                Account: state.account,
-                Contact: state.contact,
-                Contacts: state.contacts,
-                User: state.salesForceUser ? state.salesForceUser : state.user
-            }
+    },
+    locale: computed(() => {
+        if (s.state.salesForceUser) {
+            return s.state.salesForceUser.LanguageLocaleKey.split('_').join('-')
         }
-    }
-})
+        if (s.state.user) {
+            return s.state.user.LanguageLocaleKey.split('_').join('-')
+        }
+    }),
+
+    language: computed(() => {
+        if (s.state.salesForceUser) {
+            return s.state.salesForceUser.LanguageLocaleKey.split('_')[0].toLowerCase()
+        }
+        if (s.state.user) {
+            return s.state.user.LanguageLocaleKey.split('_')[0].toLowerCase()
+        }
+    }),
+    context: computed(() => {
+        return {
+            Account: s.state.account,
+            Contact: s.state.contact,
+            Contacts: s.state.contacts,
+            User: s.state.salesForceUser ? s.state.salesForceUser : s.state.user
+        }
+    })
+}
+const store = Vue.observable(s)
+
+export const useParamsStore = () => {
+    return store
+}
 
 // Params initializer
 export async function loadParams() {
-    const { state, patch } = useParamsStore()
+    const store = useParamsStore()
 
     // for testing
     if (process.env.VUE_APP_PARAMS) {
         const preParams = JSON.parse(process.env.VUE_APP_PARAMS)
-        patch(preParams)
-        return state
+        Object.assign(store.state, preParams)
+        return store.state
     }
 
     const params = await waitForWindowProp('params')
     if (params) {
-        patch(window.params)
+        Object.assign(store.state, window.params)
     }
-    return state
+    return store.state
 }
 
 export function useParams() {
@@ -100,9 +101,9 @@ window.loadPresentations = function() {}
 window.showUI = function() {}
 
 // serverJSON store
-export const useServerJSONStore = createStore({
+const b = {
     id: 'serverJSON',
-    state: () => ({
+    state: {
         files: [],
         categories: [],
         config: null,
@@ -119,27 +120,33 @@ export const useServerJSONStore = createStore({
         userfullname: null,
         ajaxtoken: null,
         isCustomerUI: false
-    })
-})
+    }
+}
+
+const storeServer = Vue.observable(b)
+
+export const useServerJSONStore = () => {
+    return storeServer
+}
 
 // serverJSON initializer
 export async function loadServerJSON(timeout = 5) {
-    const { state, patch } = useServerJSONStore()
+    const store = useServerJSONStore()
 
     // for testing
     if (process.env.VUE_APP_SERVERJSON) {
         // for testing
         const preServerJSON = JSON.parse(process.env.VUE_APP_SERVERJSON)
-        patch(preServerJSON)
-        return state
+        Object.assign(store.state, preServerJSON)
+        return store.state
     }
 
     const serverJSON = await waitForWindowProp('serverJSON', timeout)
     if (serverJSON) {
-        state.documentPath = window.documentPath
-        patch(window.serverJSON)
+        store.state.documentPath = window.documentPath
+        Object.assign(store.state, window.serverJSON)
     }
-    return state
+    return store.state
 }
 
 export function useServerJSON() {
