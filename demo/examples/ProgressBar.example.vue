6f88e011cef9350df430cff386dfe190cbf14767
Onur Elibol <korax11@hotmail.com>
2020-06-26 14:12:13 +0300

    progress bar done

---
+++ demo/examples/ProgressBar.example.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <ProgressBar :value="barModel" :total="total" @onChange="log" />
+        <!-- <ProgressBar :value="barModel" :total="total" :color="color" show-label @onChange="log">
+            <template #label="{ percent, value, total }">
+                percent: {{ percent }} value: {{ value }} total: {{ total }}
+            </template>
+        </ProgressBar> -->
+        <br />
+        <br />
+        <button @click="update">update value</button>
+        <button @click="updateTotal">update total</button>
+        <button @click="makeBlue">make blue</button>
+    </div>
+</template>
+
+<script>
+import { defineComponent, reactive, toRefs } from '@vue/composition-api'
+import ProgressBar from '@/components/ProgressBar'
+
+export default defineComponent({
+    components: {
+        ProgressBar
+    },
+    setup() {
+        const state = reactive({
+            barModel: 15,
+            total: 100,
+            color: ''
+        })
+
+        function update() {
+            state.barModel = state.barModel + 5
+        }
+
+        function updateTotal() {
+            state.total = 200
+        }
+
+        function makeBlue() {
+            state.color = 'blue'
+        }
+
+        function log(result) {
+            console.log('percent:', result.percent, 'value:', result.value, 'total:', result.total)
+        }
+
+        return { ...toRefs(state), update, updateTotal, makeBlue, log }
+    }
+})
+</script>
+
+<style lang="scss"></style>
