56235e6d65d812cc1dfc7c959166c0690e6c100c
Onur Elibol <korax11@hotmail.com>
2020-06-30 09:43:14 +0300

    lint

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -6,40 +6,39 @@
 </template>
 
 <script>
-    import { onMounted, ref, shallowRef } from '@vue/composition-api'
-    import Vue from 'vue'
-    import { createStore } from '..'
+import { onMounted, shallowRef } from '@vue/composition-api'
+import { createStore } from '..'
 
-    const useSidebarStore = () => {
-        const s = {
-            id: 'sidebar',
-            state: {
-                data: shallowRef(),
-                open: false
-            },
-            toggle() {
-                $('.ui.sidebar').sidebar('toggle')
-            },
-            show() {
-                $('.ui.sidebar').sidebar('show')
-            },
-            hide() {
-                $('.ui.sidebar').sidebar('hide')
-            }
-        }
-        return createStore(s)
-    }
-
-    export default {
-        name: 'Sidebar',
-        setup(props, ctx) {
-            const store = useSidebarStore()
-            onMounted(() => {
-                $(ctx.refs.sidebar).sidebar('setting', 'onChange', () => {
-                    store.state.open = !store.state.open
-                })
-            })
+const useSidebarStore = () => {
+    const s = {
+        id: 'sidebar',
+        state: {
+            data: shallowRef(),
+            open: false
         },
-        useSidebarStore
+        toggle() {
+            $('.ui.sidebar').sidebar('toggle')
+        },
+        show() {
+            $('.ui.sidebar').sidebar('show')
+        },
+        hide() {
+            $('.ui.sidebar').sidebar('hide')
+        }
     }
+    return createStore(s)
+}
+
+export default {
+    name: 'Sidebar',
+    setup(props, ctx) {
+        const store = useSidebarStore()
+        onMounted(() => {
+            $(ctx.refs.sidebar).sidebar('setting', 'onChange', () => {
+                store.state.open = !store.state.open
+            })
+        })
+    },
+    useSidebarStore
+}
 </script>
