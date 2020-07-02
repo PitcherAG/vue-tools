d237b065f27fbfd6e9db782bad26fee4f713bf59
Joshua Heller <joshua@torfeld6.com>
2020-07-02 11:07:20 +0200

    update gettext and i18n
    - support package translations
    - support multiple categories

--- src/gettext/config.js
+++ src/gettext/config.js
@@ -5,18 +5,55 @@ const configFile = 'gettext.config.js'
 const configPath = path.join(process.cwd(), configFile)
 
 if (!fs.existsSync(configPath)) {
-    console.error(`Config file ${configFile} not found.`)
+    console.error(`Error: file ${configFile} not found.`)
     process.exit(1)
 }
 
-module.exports = Object.assign(
-    {
-        auth: null,
-        availableLanguages: { en: 'English' },
-        category: 'app',
-        jsonBaseDir: 'public/lang',
-        poBaseDir: 'locale',
-        sourceBaseDir: 'src'
+const config = require(configPath)
+
+if (!config.languages || !config.translations.length) {
+    console.error(`Error: languages undefined or empty.`)
+    process.exit(1)
+}
+
+if (!config.translations || !config.translations.length) {
+    console.error(`Error: translations undefined or empty.`)
+    process.exit(1)
+}
+
+for (const t of config.translations) {
+    if (!['package', 'source'].includes(t.type)) {
+        console.error(`Error: invalid type ${t.type}`)
+    }
+}
+
+module.exports = config
+
+/*const defaultOptions = {
+    allCategories: function() {
+      return [this, ...this.subCategories]
     },
-    require(configPath)
-)
+    availableLanguages: {en: 'English'},
+    category: 'app',
+    importCategories: {},
+    jsonBaseDir: 'public/translations',
+    poBaseDir: 'locale',
+    sourceBaseDir: 'src',
+    sourceExcludePaths: [
+        '*-cli.js',
+    ],
+    sourceIncludePaths: [
+        '*.html',
+        '*.js',
+        '*.vue',
+    ],
+    subCategories: [],
+}
+
+const projectConfig = Object.assign({}, defaultOptions, require(configPath))
+
+for (let i = 0; i < projectConfig.subCategories.length; i++) {
+    projectConfig.subCategories[i] = Object.assign({}, projectConfig, projectConfig.subCategories[i])
+}
+
+module.exports = projectConfig*/
