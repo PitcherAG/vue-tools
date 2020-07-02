65ad4fcd893c40f0cfd433961ea735d1a4b582f4
Joshua Heller <joshua@torfeld6.com>
2020-07-02 11:26:59 +0200

    Update i18n.js

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -18,7 +18,7 @@ export const useI18nStore = () => {
     const s = {
         id: 'i18n',
         state: defaultOptions,
-        setLanguage: async function (lang, load = true, dir = 'translations', app = 'app') {
+        setLanguage: async function (lang, load = true, {app = 'app', dir = 'translations'} = {}) {
             if (!this.state.availableLanguages[lang]) {
                 throw new Error('invalid language: ' + lang)
             }
