5cb1b333213dd75f35d3170835b03827a9b0d84b
Patrick Lauber <digi@treepy.com>
2020-07-02 12:44:07 +0200

    use locale vue

--- tests/helpers/setup.js
+++ tests/helpers/setup.js
@@ -1,8 +1,4 @@
 import Vue from 'vue'
-import VueCompositionApi from '@vue/composition-api'
-import { TranslationPlugin } from '../../src'
 
 global.jQuery = global.$ = require('jquery')
 require('@pitcher/fomantic-ui')
-Vue.use(VueCompositionApi)
-Vue.use(TranslationPlugin)
