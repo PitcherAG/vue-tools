4a7a8ecea0c214bffb075df4fbb81d160da4f26f
Patrick Lauber <digi@treepy.com>
2020-07-27 17:41:37 +0200

    prototype of new store.... watcher still not working... not sure if needed

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,35 @@
 import Vue from 'vue'
-
+import { computed, watch, reactive, watchEffect } from '@vue/composition-api'
+import { arrowBind, isArrowFn } from '../utils/bind'
 const stores = {}
 
 function transform(obj) {
+    const prototype = Object.getPrototypeOf(obj)
+    const descriptors = Object.getOwnPropertyDescriptors(prototype)
+    obj = Vue.observable(obj)
     for (const prop in obj) {
-        if (typeof obj[prop] === 'function') {
-            obj[prop] = obj[prop].bind(obj)
+        if (typeof obj[prop] === 'function' && prototype.constructor.name === 'Object') {
+            if (isArrowFn(obj[prop])) {
+                obj[prop] = arrowBind(obj, obj[prop])
+            } else {
+                obj[prop] = obj[prop].bind(obj)
+            }
+        }
+        if (prop !== 'constructor' && !prop.startsWith('__') && descriptors[prop]) {
+            const get = descriptors[prop].get
+            const set = descriptors[prop].set
+            const value = descriptors[prop].value
+            if (prop.startsWith('on_')) {
+                watchEffect(value[0])
+            } else if (get && set) {
+                obj[prop] = computed({ get: get, set: set })
+            } else if (get) {
+                obj[prop] = computed(get)
+            }
         }
     }
 
-    stores[obj.id] = Vue.observable(obj)
+    stores[obj.id] = obj
 }
 
 export function createStore(obj) {
