05a04bb7a99c2e4bf3d58fc215033dc25483209c
Onur Elibol <korax11@hotmail.com>
2020-07-31 18:08:23 +0200

    grouping

---
+++ src/components/DataTable/TableRow.vue
@@ -0,0 +1,50 @@
+<template>
+    <tr>
+        <!-- If row slot exist, override with a slot -->
+        <template v-if="hasRowSlot">
+            <slot name="row" />
+        </template>
+
+        <!-- Default Row content -->
+        <template v-else v-for="f in fields">
+            <td :key="f.__colID" :class="f.tdClass">
+                <!-- if this field is a slot, get the slot -->
+                <template v-if="!!f.slotName">
+                    <slot :name="f.slotName" />
+                </template>
+                <!-- otherwise use the prop from data -->
+                <template v-else>
+                    <!-- Transform function, return mapped object, root object & field object -->
+                    {{ f.transform ? f.transform(mapper(f.dataField, item), item, f) : mapper(f.dataField, item) }}
+                </template>
+            </td>
+        </template>
+    </tr>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+import { mapper } from './table.helpers'
+
+export default defineComponent({
+    props: {
+        fields: {
+            type: Array,
+            required: true
+        },
+        item: {
+            type: Object,
+            required: true
+        },
+        hasRowSlot: {
+            default: false,
+            required: true
+        }
+    },
+    setup(props, { emit }) {
+        return { emit, mapper }
+    }
+})
+</script>
+
+<style lang="scss"></style>
