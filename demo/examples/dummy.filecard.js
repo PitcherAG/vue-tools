02ae35008ad644c07e0f448fa23eea3417f0ff89
Onur Elibol <korax11@hotmail.com>
2020-08-04 15:18:18 +0200

    dummy data

---
+++ demo/examples/dummy.filecard.js
@@ -0,0 +1,142 @@
+export default [
+    {
+        body: 'ShareDeck1',
+        category: 'presentation',
+        s3Url: 'https://btocpitcher.s3.amazonaws.com/thumbs/249549_1509373331.png',
+        keywords: '',
+        startDate: 1509460168,
+        isNew: true,
+        isFavorite: false
+    },
+    {
+        s3Url: 'https://btocpitcher.s3.amazonaws.com/thumbs/1415267145904_99313680.png',
+        keywords: 'Sys:AllowDownload',
+        startDate: '',
+        isNew: false,
+        isFavorite: true
+    },
+    {
+        s3Url: 'https://btocpitcher.s3.amazonaws.com/thumbs/1450100793180_153693233.png',
+        keywords: 'p:Edoxaban, Sys:AllowDownload',
+        startDate: 1568126347,
+        isNew: true,
+        isFavorite: false
+    },
+    {
+        s3Url: 'https://btocpitcher.s3.amazonaws.com/thumbs/-Lo-oIt-AnDufzOPYhcD.png',
+        keywords: 'c:AP, Sys:PContent',
+        startDate: 1568126209,
+        isNew: false,
+        isFavorite: true
+    },
+    {
+        s3Url: 'https://btocpitcher.s3.amazonaws.com/thumbs/232793_1503650029.png',
+        keywords: 'Epic Tool, p:Epic',
+        startDate: 1505490292
+    },
+    {
+        s3Url: 'https://btocpitcher.s3.amazonaws.com/thumbs/125229_1460012085.png',
+        keywords: 'p:XBan',
+        startDate: 1460014396
+    },
+    {
+        s3Url: 'https://btocpitcher.s3.amazonaws.com/thumbs/742391_1576688589.png',
+        keywords: 'Sys:PContent, p:Test',
+        startDate: 1584691106
+    },
+    {
+        s3Url: 'https://btocpitcher.s3.amazonaws.com/thumbs/38959_1389180808.png',
+        keywords: 'p:test',
+        startDate: 1543420909
+    },
+    {
+        s3Url: 'https://btocpitcher.s3.amazonaws.com/thumbs/-LoQnVA7OPGQQuc7PWKp.png',
+        keywords: 'p:Test, Sys:AllowDownload',
+        startDate: 1568204175
+    },
+    {
+        s3Url: 'https://btocpitcher.s3.amazonaws.com/thumbs/1415968627376_100441030.png',
+        keywords: '',
+        startDate: ''
+    },
+    {
+        s3Url: 'https://btocpitcher.s3.amazonaws.com/thumbs/655628_1568129616.png',
+        keywords: 'p:Test',
+        startDate: 1568204175
+    },
+    {
+        s3Url: 'https://btocpitcher.s3.amazonaws.com/thumbs/249549_1509373331.png',
+        keywords: 'p:gma, Sys:AllowDownload',
+        startDate: 1509460168
+    },
+    {
+        s3Url: 'https://btocpitcher.s3.amazonaws.com/thumbs/64071_1415968632.png',
+        keywords: '',
+        startDate: ''
+    },
+    {
+        s3Url: 'https://btocpitcher.s3.amazonaws.com/thumbs/104391_1447319648.png',
+        keywords: 'p:test4',
+        startDate: 1447326973
+    },
+    {
+        s3Url: 'https://btocpitcher.s3.amazonaws.com/thumbs/483304_1543508641.png',
+        keywords: 'preskey=gma, o:3, p:gma, Sys:AllowDownload, SpecialCode:VerySpeciallCode',
+        startDate: 1568204175
+    },
+    {
+        s3Url: 'https://btocpitcher.s3.amazonaws.com/thumbs/492523_1545038313.png',
+        keywords: 'preskey=Test, p:jtest',
+        startDate: 1568204175
+    },
+    {
+        s3Url: 'https://btocpitcher.s3.amazonaws.com/thumbs/82917_1429190315.png',
+        keywords: 'p:test3',
+        startDate: 1438939314
+    },
+    {
+        s3Url: 'https://btocpitcher.s3.amazonaws.com/thumbs/192101_1489932792.png',
+        keywords: 'c:chocolate, p:Test',
+        startDate: 1494567400
+    },
+    {
+        s3Url: 'https://btocpitcher.s3.amazonaws.com/thumbs/492524_1545038889.png',
+        keywords: 'preskey=Test, p:jtest',
+        startDate: 1568204175
+    },
+    {
+        body: '0509',
+        category: 'presentation',
+        s3Url: 'https://btocpitcher.s3.amazonaws.com/thumbs/492524_1545038889.png',
+        keywords: '',
+        startDate: 1568204175
+    },
+    {
+        body: 'CustomDeckT1',
+        category: 'presentation',
+        s3Url: 'https://btocpitcher.s3.amazonaws.com/thumbs/192101_1489932792.png',
+        keywords: '',
+        startDate: 1494567400
+    },
+    {
+        body: 'CustomEditTest1',
+        category: 'presentation',
+        s3Url: 'https://btocpitcher.s3.amazonaws.com/thumbs/192101_1489932792.png',
+        keywords: '',
+        startDate: 1494567400
+    },
+    {
+        body: 'CustomEditTest2',
+        category: 'presentation',
+        s3Url: 'https://btocpitcher.s3.amazonaws.com/thumbs/192101_1489932792.png',
+        keywords: '',
+        startDate: 1494567400
+    },
+    {
+        body: 'new custom',
+        category: 'presentation',
+        s3Url: 'https://btocpitcher.s3.amazonaws.com/thumbs/492524_1545038889.png',
+        keywords: '',
+        startDate: 1568204175
+    }
+]
