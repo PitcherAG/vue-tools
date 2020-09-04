6125d755567f861132083b4d7552d099c7930434
Greg Kotula <greg@greg>
2020-09-04 11:47:21 +0200

    Load composition api in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,3 +2,8 @@
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+import Vue from 'vue'
+import VueCompositionApi from '@vue/composition-api'
+
+Vue.use(VueCompositionApi)
