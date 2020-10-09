5d50443187c625d7c8162dc0b74c36adc240e4d8
elibolonur <elibolonur@users.noreply.github.com>
2020-10-09 15:51:17 +0000

    Deploying to feature-docs-storybook-new-kouts from  @ cc780506a68f37f214df55de29e6f66f24eae6ef 🚀

--- src/utils/assign.js
+++
@@ -1,8 +0,0 @@
-export function assign(source, target) {
-    console.warn('deprecated')
-    for (const a in source) {
-        if (Object.prototype.hasOwnProperty.call(target, a)) {
-            target[a] = source[a]
-        }
-    }
-}
