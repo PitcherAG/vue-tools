6abbc8d0bf4bdcf76e34c291f6036b3a0181b262
kouts <koutsaftakis@gmail.com>
2020-10-16 16:59:12 +0300

    Updated docs added load more example

--- stories/components/data/filter-dropdown.data.js
+++ stories/components/data/filter-dropdown.data.js
@@ -44,3 +44,306 @@ export const options = [
         description: 44
     }
 ]
+
+export const names = [
+    {
+        value: 38,
+        text: 'Henry Brown'
+    },
+    {
+        value: 39,
+        text: 'Cody Harris'
+    },
+    {
+        value: 40,
+        text: 'Henrietta Gonzalez'
+    },
+    {
+        value: 41,
+        text: 'Jorge Kelly'
+    },
+    {
+        value: 42,
+        text: 'Nicholas Murphy'
+    },
+    {
+        value: 43,
+        text: 'Donald McKenzie'
+    },
+    {
+        value: 44,
+        text: 'Donald Parsons'
+    },
+    {
+        value: 45,
+        text: 'Russell Curtis'
+    },
+    {
+        value: 46,
+        text: 'Helena Wise'
+    },
+    {
+        value: 47,
+        text: 'Jorge Holt'
+    },
+    {
+        value: 48,
+        text: 'Donald Curry'
+    },
+    {
+        value: 49,
+        text: 'Annie Garner'
+    },
+    {
+        value: 50,
+        text: 'Ruth Dean'
+    },
+    {
+        value: 51,
+        text: 'Delia Harvey'
+    },
+    {
+        value: 52,
+        text: 'Jeffrey Pittman'
+    },
+    {
+        value: 53,
+        text: 'Florence Watts'
+    },
+    {
+        value: 54,
+        text: 'Aiden Roberson'
+    },
+    {
+        value: 55,
+        text: 'Curtis Garrett'
+    },
+    {
+        value: 56,
+        text: 'Adele Bennett'
+    },
+    {
+        value: 57,
+        text: 'Nelle Day'
+    },
+    {
+        value: 58,
+        text: 'Jorge Crawford'
+    },
+    {
+        value: 59,
+        text: 'Ronald Andrews'
+    },
+    {
+        value: 60,
+        text: 'Ronald Parks'
+    },
+    {
+        value: 61,
+        text: 'Hulda Price'
+    },
+    {
+        value: 62,
+        text: 'Tony Spencer'
+    },
+    {
+        value: 63,
+        text: 'Cameron Gibson'
+    },
+    {
+        value: 64,
+        text: 'Elijah Warren'
+    },
+    {
+        value: 65,
+        text: 'Willie Fleming'
+    },
+    {
+        value: 66,
+        text: 'Luke Weber'
+    },
+    {
+        value: 67,
+        text: 'Dustin Flowers'
+    },
+    {
+        value: 68,
+        text: 'Minerva Paul'
+    },
+    {
+        value: 69,
+        text: 'Esther Mack'
+    },
+    {
+        value: 70,
+        text: 'Hulda Roberts'
+    },
+    {
+        value: 71,
+        text: 'Effie French'
+    },
+    {
+        value: 72,
+        text: 'Cole Gutierrez'
+    },
+    {
+        value: 73,
+        text: 'Edna Lawson'
+    },
+    {
+        value: 74,
+        text: 'Roxie Adams'
+    },
+    {
+        value: 75,
+        text: 'Eugenia Payne'
+    },
+    {
+        value: 76,
+        text: 'Albert Bradley'
+    },
+    {
+        value: 77,
+        text: 'Harriet Rogers'
+    },
+    {
+        value: 78,
+        text: 'Viola Alvarado'
+    },
+    {
+        value: 79,
+        text: 'Lida Gibbs'
+    },
+    {
+        value: 80,
+        text: 'Hattie Colon'
+    },
+    {
+        value: 81,
+        text: 'Russell Martin'
+    },
+    {
+        value: 82,
+        text: 'Estella Buchanan'
+    },
+    {
+        value: 83,
+        text: 'Victoria Sanchez'
+    },
+    {
+        value: 84,
+        text: 'Lucy Wallace'
+    },
+    {
+        value: 85,
+        text: 'Sara Rodriquez'
+    },
+    {
+        value: 86,
+        text: 'Myra Hogan'
+    },
+    {
+        value: 87,
+        text: 'Harold Diaz'
+    },
+    {
+        value: 88,
+        text: 'Landon Bowen'
+    },
+    {
+        value: 89,
+        text: 'Marguerite Turner'
+    },
+    {
+        value: 90,
+        text: 'Donald Glover'
+    },
+    {
+        value: 91,
+        text: 'Madge Lambert'
+    },
+    {
+        value: 92,
+        text: 'May Shelton'
+    },
+    {
+        value: 93,
+        text: 'Frank Lucas'
+    },
+    {
+        value: 94,
+        text: 'William Schneider'
+    },
+    {
+        value: 95,
+        text: 'Austin Yates'
+    },
+    {
+        value: 96,
+        text: 'Gene Figueroa'
+    },
+    {
+        value: 97,
+        text: 'Leo Malone'
+    },
+    {
+        value: 98,
+        text: 'Hilda Carr'
+    },
+    {
+        value: 99,
+        text: 'Bettie Holmes'
+    },
+    {
+        value: 100,
+        text: 'Roger Dixon'
+    },
+    {
+        value: 101,
+        text: 'Georgie Andrews'
+    },
+    {
+        value: 102,
+        text: 'Hulda Wilkerson'
+    },
+    {
+        value: 103,
+        text: 'Lora Manning'
+    },
+    {
+        value: 104,
+        text: 'Minnie Sherman'
+    },
+    {
+        value: 105,
+        text: 'Jordan Hoffman'
+    },
+    {
+        value: 106,
+        text: 'Timothy Cummings'
+    },
+    {
+        value: 107,
+        text: 'Elijah Rhodes'
+    },
+    {
+        value: 108,
+        text: 'Steven Bass'
+    },
+    {
+        value: 109,
+        text: 'Lela Saunders'
+    },
+    {
+        value: 110,
+        text: 'Sophia Flores'
+    },
+    {
+        value: 111,
+        text: 'Rhoda Norton'
+    },
+    {
+        value: 112,
+        text: 'Ada Gutierrez'
+    }
+]
