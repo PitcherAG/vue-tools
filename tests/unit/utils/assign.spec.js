9e8ca0a76bf2bae8c12325e5e275164dab91be46
kouts <koutsaftakis@gmail.com>
2020-11-06 10:22:30 +0200

    Added test for assing method

---
+++ tests/unit/utils/assign.spec.js
@@ -0,0 +1,36 @@
+import { assign } from '@/utils/assign'
+
+const data = {
+    one: 'a',
+    two: 'b',
+    three: 'c',
+    four: 'd'
+}
+
+describe('assign util', () => {
+    it('assigns values only if they exist in the target object', () => {
+        const obj = {
+            one: 'test',
+            foo: 'bar'
+        }
+        assign(data, obj)
+        expect(obj).toEqual({
+            one: 'a',
+            foo: 'bar'
+        })
+    })
+
+    it('does not assign values to an empty object', () => {
+        const obj = {}
+        assign(data, obj)
+        expect(obj).toEqual({})
+    })
+
+    it('returns undefined when used as a data assignment method', () => {
+        const obj = {
+            one: 'a'
+        }
+        const res = assign(data, obj)
+        expect(res).toBeUndefined()
+    })
+})
