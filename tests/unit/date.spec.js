1dac5c1544d3c58706a4d1131c24d6487d728568
Greg Kotula <greg@greg>
2020-11-11 23:19:10 +0100

    Fixing tests

--- tests/unit/date.spec.js
+++ tests/unit/date.spec.js
@@ -1,4 +1,4 @@
-import { formatDate, useI18nStore } from '@/'
+import { formatDate, useParamsStore } from '@/'
 import { createLocalVue } from '@vue/test-utils'
 import CompositionApi from '@vue/composition-api'
 const localVue = createLocalVue()
@@ -8,17 +8,15 @@ jest.mock('@/i18n/i18n')
 
 describe('i18n', () => {
     it('date', () => {
-        let resp = { state: { locale: 'de-CH' } }
-        useI18nStore.mockReturnValue(resp)
+        const params = useParamsStore()
+        params.state.user = { LocaleSidKey: 'de-CH' }
         expect(formatDate('2020-02-20')).toBe('20.02.20')
-        resp = { state: { locale: 'en-US' } }
-        useI18nStore.mockReturnValue(resp)
+        params.state.user = { LocaleSidKey: 'en-US' }
         expect(formatDate('2020-05-11T19:12:11.000+0000')).toBe('05/11/20')
         expect(formatDate('2020-02-20')).toBe('02/20/20')
         expect(formatDate()).toBe('')
         expect(formatDate('abcdfef')).toBe('Invalid Date')
-        resp = { state: { locale: null } }
-        useI18nStore.mockReturnValue(resp)
+        params.state.user = { LocaleSidKey: null }
         expect(() => {
             formatDate('2020-02-20')
         }).toThrow()
