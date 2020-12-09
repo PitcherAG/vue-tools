df73a5e4933c3964b2c4703de7474e4605457c07
Onur Elibol <onur.elibol@pitcher.com>
2020-12-09 15:38:07 +0100

    parse objects as string

--- tests/helpers/setup.js
+++ tests/helpers/setup.js
@@ -3,6 +3,7 @@ import { TI, SERVER_JSON, PRESENTATIONS_OBJECT } from './system-dummy-data'
 global.jQuery = global.$ = require('jquery')
 require('@pitcher/fomantic-ui')
 
-process.env.VUE_APP_TI = TI
-process.env.VUE_APP_SERVERJSON = SERVER_JSON
-process.env.VUE_APP_PRESENTATIONSOBJECT = PRESENTATIONS_OBJECT
+// Setup Ti related stuff
+process.env.VUE_APP_TI = JSON.stringify(TI)
+process.env.VUE_APP_SERVERJSON = JSON.stringify(SERVER_JSON)
+process.env.VUE_APP_PRESENTATIONSOBJECT = JSON.stringify(PRESENTATIONS_OBJECT)
