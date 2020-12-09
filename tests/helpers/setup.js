b90e5ca6eaecbed8bec36d9f69d3826a1092778a
Onur Elibol <onur.elibol@pitcher.com>
2020-12-09 15:34:23 +0100

    add env stuff to setup

--- tests/helpers/setup.js
+++ tests/helpers/setup.js
@@ -1,2 +1,8 @@
+import { TI, SERVER_JSON, PRESENTATIONS_OBJECT } from './system-dummy-data'
+
 global.jQuery = global.$ = require('jquery')
 require('@pitcher/fomantic-ui')
+
+process.env.VUE_APP_TI = TI
+process.env.VUE_APP_SERVERJSON = SERVER_JSON
+process.env.VUE_APP_PRESENTATIONSOBJECT = PRESENTATIONS_OBJECT
