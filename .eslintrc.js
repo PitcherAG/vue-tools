05e89b19e574647ec793802a2c3ab2ece2d025d0
Onur Elibol <onur.elibol@pitcher.com>
2020-12-11 13:34:04 +0100

    lint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,10 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint'
   },
+  rules: {
+    'vue/custom-event-name-casing': 0,
+    'vue/no-static-inline-styles': 0
+  },
   overrides: [
     {
       files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
--- src/components/FileCard/FileCardContainer.vue
+++ src/components/FileCard/FileCardContainer.vue
@@ -5,7 +5,9 @@
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'file-card-container'
+}
 </script>
 
 <style lang="scss" scoped>
