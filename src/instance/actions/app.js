7ddcef8e12fe0a2f79753c1cf19adcdf6353b775
yasat k bilgin <pitcher13@yasats-MacBook-Pro-2.local>
2020-12-15 15:17:01 +0300

    add options to exitApp

--- src/instance/actions/app.js
+++ src/instance/actions/app.js
@@ -1,12 +1,12 @@
 import { fireEvent } from '../../event'
 
-export function exitApp() {
-  fireEvent('exitApp', {})
+export function exitApp(options) {
+    fireEvent('exitApp', options || {})
 }
 
 export function showAlertBox(title, message) {
-  fireEvent('showAlertBox', {
-    title: title,
-    message: message
-  })
+    fireEvent('showAlertBox', {
+        title: title,
+        message: message
+    })
 }
