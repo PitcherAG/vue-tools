0082756d77a33be0db81150b105ca047fcf0d4ea
Mustafa Çağrı Güven <mguven@sabanciuniv.edu>
2020-12-20 13:10:06 +0300

    Added some features to Progress Bar
    Alternative colors and increase & decrease buttons added to progress bar

--- demo/examples/ProgressBar.example.vue
+++ demo/examples/ProgressBar.example.vue
@@ -6,11 +6,18 @@
                 percent: {{ percent }} value: {{ value }} total: {{ total }}
             </template>
         </ProgressBar> -->
-    <br />
-    <br />
-    <button @click="update">update value</button>
-    <button @click="updateTotal">update total</button>
-    <button @click="makeBlue">make blue</button>
+        
+        <div class="selectors">
+            <button @click="decrease">decrease value</button>
+            <button @click="increase">increase value</button>
+            <button @click="updateTotal">update total</button>
+            <button @click="makeBlue">make blue</button>
+
+            <select v-model="color">
+                <option disabled>pick a color</option>
+                <option v-for="clr in colors" :key="clr.id">{{clr}}</option>
+            </select>
+        </div>
   </div>
 </template>
 
@@ -25,13 +32,24 @@ export default defineComponent({
   },
   setup() {
     const state = reactive({
-      barModel: 15,
-      total: 100,
-      color: 'blue'
+        barModel: 15,
+        counter: 5,
+        total: 100,
+        min: 10,
+        color: 'blue',
+        colors: ['blue', 'purple', 'orange', 'red', 'green', 'black']
     })
 
-    function update() {
-      state.barModel = state.barModel + 5
+    function decrease() {
+        if(state.min+state.counter<=state.barModel){
+            state.barModel = state.barModel - state.counter
+        }
+    }
+
+    function increase() {
+        if(state.total-state.counter>=state.barModel){
+            state.barModel = state.barModel + state.counter
+        }
     }
 
     function updateTotal() {
@@ -46,9 +64,13 @@ export default defineComponent({
       console.log('percent:', result.percent, 'value:', result.value, 'total:', result.total)
     }
 
-    return { ...toRefs(state), update, updateTotal, makeBlue, log }
+    return { ...toRefs(state), increase, decrease, updateTotal, makeBlue, log }
   }
 })
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+    .selectors {margin-top: 30px;
+        select:not(:first-child), button:not(:first-child) {margin-left:5px;}
+    }
+</style>
--- tests/unit/progressbar.spec.js
+++ tests/unit/progressbar.spec.js
@@ -16,7 +16,9 @@ describe('ProgressBar.vue', () => {
     localVue,
     propsData: {
       value: 10,
-      showLabel: false
+      showLabel: false,
+      color: 'red',
+      maxWidth: '100%'
     }
   })
 
@@ -45,5 +47,15 @@ describe('ProgressBar.vue', () => {
     // need to delay 1 sec as it animates the text inside as well
     await delay(1000)
     expect(wrapper.find('.ui.progress .progress').text()).toBe(`${wrapper.vm.$props.value}%`)
+  })
+
+  it(`Progress Bar's container initialize correctly`, () => {
+      // checks all props in progress container div and no need to await since animations are not important
+      expect(wrapper.html()).toContain(`<div data-value=\"${wrapper.vm.$props.value}\" data-total=\"${wrapper.vm.$props.total}\" class=\"ui progress pitcher-progress ${wrapper.vm.$props.color}\" style=\"max-width: ${wrapper.vm.$props.maxWidth};\" data-percent=\"${wrapper.vm.$props.value}\">`)
+  })
+
+  it(`Updating Progress Bar's color works`, async () => {
+      await updateValue('color', 'brown')
+      expect(wrapper.html()).toContain(`<div data-value=\"${wrapper.vm.$props.value}\" data-total=\"${wrapper.vm.$props.total}\" class=\"ui progress pitcher-progress brown\" style=\"max-width: ${wrapper.vm.$props.maxWidth};\" data-percent=\"${wrapper.vm.$props.value}\">`)
   })
 })
