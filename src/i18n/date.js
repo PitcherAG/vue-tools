5e5cfd16ee2102ff83684be32361e0a640f95570
Onur Elibol <onur.elibol@pitcher.com>
2020-12-28 17:25:44 +0100

    add fallback to formatting date & numbers

--- src/i18n/date.js
+++ src/i18n/date.js
@@ -1,19 +1,22 @@
-import { useParamsStore } from '../params'
 import moment from 'moment'
+import { useParamsStore } from '../params'
+import { useI18nStore } from '../i18n/i18n'
 
 export function formatDate(date, showYear = true) {
-  const params = useParamsStore()
-  if (!date) {
-    return ''
-  }
+  const locale = useParamsStore().locale || useI18nStore().state.locale
+
+  if (!date) return ''
+
   let options
+
   if (showYear) {
     options = { year: '2-digit', month: '2-digit', day: '2-digit' }
   } else {
     options = { month: 'short', day: '2-digit' }
   }
-  if (!params.locale) throw 'locale not defined'
+
+  if (!locale) throw 'locale not defined'
   const m = moment(date)
   const d = m.toDate()
-  return d.toLocaleDateString(params.locale, options)
+  return d.toLocaleDateString(locale, options)
 }
