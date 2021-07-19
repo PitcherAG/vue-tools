56262ce9644cb4c24647ce6223ee3fd08295161c
Onur Elibol <onur.elibol@pitcher.com>
2021-07-19 17:07:52 +0200

    feat: add auto release process

---
+++ release.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  branches: [{ name: 'feat/release-automation' }],
+  dryRun: false,
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
+    ['@semantic-release/npm', { npmPublish: true, pkgRoot: '.' }],
+    [
+      '@semantic-release/github',
+      {
+        successComment: false,
+        failComment: false,
+        releasedLabels: [
+          // eslint-disable-next-line max-len
+          'released<%= nextRelease.channel ? ` on ${nextRelease.channel}@${nextRelease.version}` : "" %> from <%= branch.name %>',
+        ],
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md'],
+        message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+}
